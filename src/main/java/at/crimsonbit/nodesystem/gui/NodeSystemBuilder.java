95302cb211bc7edb4b4af354261fbacfc8cb4287
NeonArtworks <flori.wagner@hotmail.com>
2018-04-26 21:34:56 +0200

    some comments

--- src/main/java/at/crimsonbit/nodesystem/gui/NodeSystemBuilder.java
+++ src/main/java/at/crimsonbit/nodesystem/gui/NodeSystemBuilder.java
@@ -5,6 +5,16 @@ import java.util.logging.Level;
 import at.crimsonbit.nodesystem.util.logger.SystemLogger;
 import javafx.scene.Scene;
 
+/**
+ * <h1>NodeSystemBuilder</h1>
+ * <p>
+ * The NodeSystemBuilder class allows the user to create/build the GNodeGraph
+ * object.
+ * </p>
+ * 
+ * @author Florian Wagner
+ *
+ */
 public class NodeSystemBuilder {
 
 	private GNodeSystem sys;
@@ -14,12 +24,31 @@ public class NodeSystemBuilder {
 	private double w;
 	private double h;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param width
+	 *            width of the nodegraph scene
+	 * @param height
+	 *            height of the nodegraph scene
+	 */
 	public NodeSystemBuilder(double width, double height) {
 		this.w = width;
 		this.h = height;
 
 	}
 
+	/**
+	 * <h1>init()</h1>
+	 * <p>
+	 * Initializes the NodeSystemBuilder. This has to be called before all other
+	 * methods with the exception of the {@link NodeSystemBuilder#attachLogger()}
+	 * function. If you want to attach the logger to the nodegraph, you have to call
+	 * it before the {@link NodeSystemBuilder#init()} method!
+	 * </p>
+	 * 
+	 * @return this
+	 */
 	public NodeSystemBuilder init() {
 		sys = new GNodeSystem();
 		view = sys.getNodeView();
@@ -29,6 +58,15 @@ public class NodeSystemBuilder {
 		return this;
 	}
 
+	/**
+	 * <h1>attachSettingsPane()</h1>
+	 * <p>
+	 * Attaches a settings pane to the nodegraph. Currently a work in progress ->
+	 * DOES NOTHING!
+	 * </p>
+	 * 
+	 * @return this
+	 */
 	public NodeSystemBuilder attachSettingsPane() {
 		sys.attachSettingsPane(true);
 		view = sys.getNodeView();
@@ -38,6 +76,18 @@ public class NodeSystemBuilder {
 		return this;
 	}
 
+	/**
+	 * <h1>registerCustomNodes({@link String}</h1>
+	 * <p>
+	 * This method can be called as many times as neccessary! With this method you
+	 * can register your own custom nodes by telling this method the package as
+	 * string of your nodes.
+	 * </p>
+	 * 
+	 * @param pack
+	 *            the package name as string
+	 * @return this
+	 */
 	public NodeSystemBuilder registerCustomNodes(String pack) {
 		if (graph != null)
 			graph.registerNodes(pack);
@@ -45,6 +95,16 @@ public class NodeSystemBuilder {
 		return this;
 	}
 
+	/**
+	 * <h1>attachLogger()</h1>
+	 * <p>
+	 * Attaches a logger to the nodegraph. <br>
+	 * Attention: You have to call this method <b>BEFORE</b>
+	 * {@link NodeSystemBuilder#init()}!
+	 * </p>
+	 * 
+	 * @return this
+	 */
 	public NodeSystemBuilder attachLogger() {
 
 		SystemLogger.attachLogger();
@@ -52,6 +112,17 @@ public class NodeSystemBuilder {
 		return this;
 	}
 
+	/**
+	 * <h1>registerDefaultNodes({@link Boolean}</h1>
+	 * <p>
+	 * Registers the default nodes of the nodegraph.
+	 * </p>
+	 * 
+	 * @param f
+	 *            if you want to register the default nodes, pass true, otherwise
+	 *            false
+	 * @return this
+	 */
 	public NodeSystemBuilder registerDefaultNodes(boolean f) {
 		if (graph != null)
 			graph.initGraph(f);
@@ -59,12 +130,29 @@ public class NodeSystemBuilder {
 		return this;
 	}
 
+	/**
+	 * <h1>attachInfo()</h1>
+	 * <p>
+	 * Attaches additional information about the nodes, and the nodegraph, to the
+	 * graph as text.
+	 * </p>
+	 * 
+	 * @return this
+	 */
 	public NodeSystemBuilder attachInfo() {
 		if (graph != null)
 			graph.addInfo();
 		return this;
 	}
 
+	/**
+	 * <h1>build()</h1>
+	 * <p>
+	 * Builds the nodegraph. It returns the {@link GNodeGraph} which was created
+	 * using the build/ attach methods of this class.
+	 * 
+	 * @return the newly built GNodeGraph object
+	 */
 	public GNodeGraph build() {
 		this.graph.log(Level.INFO, "NodeSystem ready!");
 		return this.graph;
